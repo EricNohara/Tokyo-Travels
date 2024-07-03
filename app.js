826915ba8c8a3f724237e5d04f62566a9f57fb10
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-07-03 12:38:34 -0500

    added code from QA section of book

--- app.js
+++ app.js
@@ -9,12 +9,29 @@ app.set("view engine", "handlebars");
 app.set("port", process.env.PORT || 8000);
 app.use(express.static(__dirname + "/public"));
 
+app.use(function (req, res, next) {
+  res.locals.showTests =
+    app.get("env") !== "production" && req.query.test === "1";
+  next();
+});
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { fortune: fortune.getFortune() });
+  res.render("about", {
+    fortune: fortune.getFortune(),
+    pageTestScript: "/qa/tests-about.js",
+  });
+});
+
+app.get("/tours/sumida-river", (req, res) => {
+  res.render("tours/sumida-river");
+});
+
+app.get("/tours/request-group-rate", (req, res) => {
+  res.render("tours/request-group-rate");
 });
 
 app.use((req, res) => {
---
+++ public/qa/tests-about.js
@@ -0,0 +1,7 @@
+const assert = require("chai").assert;
+
+suite('"About" Page Tests', function () {
+  test("page should contain link to contact page", function () {
+    assert($('a[href="/contact"]').length);
+  });
+});
